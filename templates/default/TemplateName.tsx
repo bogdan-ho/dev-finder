b5cce4ec317481867d4027b7d5f9b946b60c88ab
Bogdan <homoretsky@gmail.com>
2023-02-27 16:51:18 +0500

    add generate-react-cli configs and templates

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './TemplateName.module.scss';
+
+interface TemplateNameProps { }
+
+export const TemplateName = ({ }: TemplateNameProps) => (
+  <div className={styles.TemplateName} data-testid="TemplateName">
+    TemplateName Component
+  </div>
+);
