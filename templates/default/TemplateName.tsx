00e85fc245f103a90f0438c3e81f48d282739e1f
Bogdan <homoretsky@gmail.com>
2023-02-27 19:09:16 +0500

    update generate-react-cli template

--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -3,8 +3,10 @@ import styles from './TemplateName.module.scss';
 
 interface TemplateNameProps { }
 
-export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
+const TemplateName = ({ }: TemplateNameProps) => (
+  <div className={styles.templateName} data-testid="TemplateName">
     TemplateName Component
   </div>
 );
+
+export default TemplateName;
