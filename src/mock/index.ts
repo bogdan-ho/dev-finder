e87573a747ef6adb66d82ff4b80e6312fe6e8c81
Bogdan <homoretsky@gmail.com>
2023-02-28 19:34:10 +0500

    add defaultUser mock

---
+++ src/mock/index.ts
@@ -0,0 +1,19 @@
+import { LocalGithubUser } from '../types';
+
+const defaultUser: LocalGithubUser = {
+  login: 'bogdan-ho',
+  id: 100417483,
+  avatar: 'https://avatars.githubusercontent.com/u/100417483?v=4',
+  name: 'Bogdan Homoretsky',
+  company: null,
+  blog: 'https://t.me/bogdanho',
+  location: 'Russia, Beloretsk',
+  bio: 'Frontend React Developer 🦾 with skills of routine processes automatization⚡️',
+  twitter: 'bogdankho',
+  repos: 30,
+  followers: 1,
+  following: 1,
+  created: '2022-02-25T11:54:46Z',
+};
+
+export default defaultUser;
