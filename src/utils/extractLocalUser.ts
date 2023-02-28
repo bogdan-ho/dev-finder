107adfad0eb0fe4b62dca26f4a05b5fbeccdbbd0
Bogdan <homoretsky@gmail.com>
2023-02-28 21:30:27 +0500

    add extractLocalUser func to utils

---
+++ src/utils/extractLocalUser.ts
@@ -0,0 +1,19 @@
+import { GithubUser, LocalGithubUser } from '../types';
+
+const extractLocalUser = (user: GithubUser): LocalGithubUser => ({
+  login: user.login,
+  avatar: user.avatar_url,
+  name: user.name,
+  bio: user.bio,
+  blog: user.blog,
+  company: user.company,
+  created: user.created_at,
+  followers: user.followers,
+  following: user.following,
+  location: user.location,
+  repos: user.public_repos,
+  twitter: user.twitter_username,
+  id: user.id,
+});
+
+export default extractLocalUser;
